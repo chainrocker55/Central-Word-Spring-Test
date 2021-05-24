2781d8910bb8b102358834314efa5d1aae0d53e7
chainrocker55 <chainrock55@gmail.com>
2021-05-24 14:41:22 +0700

    Update Unit test PriductServicePort

--- CentralGroupHexagonalAPI/domain/src/test/java/org/example/service/ProductServiceImplTest.java
+++ CentralGroupHexagonalAPI/domain/src/test/java/org/example/service/ProductServiceImplTest.java
@@ -1,15 +1,17 @@
 package org.example.service;
 
 import org.example.data.ProductDto;
-import org.junit.jupiter.api.Disabled;
+import org.example.ports.spi.ProductPersistencePort;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -17,10 +19,12 @@ import static org.junit.Assert.assertEquals;
 class ProductServiceImplTest {
 
     @Mock
+    private ProductPersistencePort productPersistencePort;
+
+    @InjectMocks
     private ProductServiceImpl productService;
 
 
-    @Disabled
     @Test
     void addProduct_returnProduct(){
         ProductDto productDto = new ProductDto();
@@ -29,11 +33,57 @@ class ProductServiceImplTest {
         productDto.setDescription("Description");
         productDto.setPrice(100.0);
 
+        when(productPersistencePort.addProduct(productDto)).thenReturn(productDto);
         ProductDto productDtoReturn = productService.addProduct(productDto);
+
+        verify(productPersistencePort,atLeastOnce()).addProduct(productDto);
         assertEquals(productDto.getId(),productDtoReturn.getId());
         assertEquals(productDto.getName(),productDtoReturn.getName());
         assertEquals(productDto.getDescription(),productDtoReturn.getDescription());
         assertEquals(productDto.getPrice(),productDtoReturn.getPrice());
+    }
+    @Test
+    void deleteProduct_returnProduct(){
+        doNothing().when(productPersistencePort).deleteProductById(1);
+        productService.deleteProductById(1);
+        verify(productPersistencePort,atLeastOnce()).deleteProductById(1);
+
+    }
+    @Test
+    void updateProduct_returnProduct(){
+        ProductDto productDto = new ProductDto();
+        productDto.setId(1);
+        productDto.setName("Test");
+        productDto.setDescription("Description");
+        productDto.setPrice(100.0);
+
+        when(productPersistencePort.updateProduct(productDto)).thenReturn(productDto);
+        ProductDto productDtoReturn = productService.updateProduct(productDto);
+
+        verify(productPersistencePort,atLeastOnce()).updateProduct(productDto);
+        assertEquals(productDto.getId(),productDtoReturn.getId());
+        assertEquals(productDto.getName(),productDtoReturn.getName());
+        assertEquals(productDto.getDescription(),productDtoReturn.getDescription());
+        assertEquals(productDto.getPrice(),productDtoReturn.getPrice());
+
+    }
+    @Test
+    void getProductById_returnProduct(){
+        ProductDto productDto = new ProductDto();
+        productDto.setId(1);
+        productDto.setName("Test");
+        productDto.setDescription("Description");
+        productDto.setPrice(100.0);
+
+        when(productPersistencePort.getProductById(1)).thenReturn(productDto);
+        ProductDto productDtoReturn = productService.getProductById(1);
+
+        verify(productPersistencePort,atLeastOnce()).getProductById(1);
+        assertEquals(productDto.getId(),productDtoReturn.getId());
+        assertEquals(productDto.getName(),productDtoReturn.getName());
+        assertEquals(productDto.getDescription(),productDtoReturn.getDescription());
+        assertEquals(productDto.getPrice(),productDtoReturn.getPrice());
+
     }
 
 }
